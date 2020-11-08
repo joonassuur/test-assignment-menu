b54788e5572b3dfd0f97ea3c88b226e8dd7bb54a
Joonas <joonas.suurvali@gmail.com>
2020-11-08 11:52:52 +0200

    added an additional test

--- src/components/dropdown-main/DropdownMain.test.js
+++ src/components/dropdown-main/DropdownMain.test.js
@@ -1,13 +1,12 @@
 import React from "react";
 import TestRenderer from "react-test-renderer";
-
+import DropdownGeneral from "./dropdown-general/DropdownGeneral";
+import DropdownAchievements from "./dropdown-achievements/DropdownAchievements";
 import DropdownMain from "./DropdownMain";
 
-const mockDispatch = jest.fn();
-
 jest.mock("react-redux", () => ({
-  useSelector: jest.fn((fn) => fn()),
-  useDispatch: () => mockDispatch,
+  useSelector: jest.fn(),
+  useDispatch: jest.fn(),
 }));
 
 jest.mock("../../redux/Index");
@@ -16,5 +15,21 @@ describe("DropdownMain component", () => {
   test("Matches the snapshot", () => {
     const dropdownMain = TestRenderer.create(<DropdownMain />);
     expect(dropdownMain.toJSON()).toMatchSnapshot();
+  });
+
+  test("Render correct component according to state", () => {
+    let activeDD = "achievements";
+
+    const renderActiveDD = () => {
+      if (activeDD === "achievements") {
+        return <DropdownAchievements />;
+      }
+      return <DropdownGeneral />;
+    };
+    expect(renderActiveDD()).toStrictEqual(<DropdownAchievements />);
+
+    activeDD = "general";
+
+    expect(renderActiveDD()).toStrictEqual(<DropdownGeneral />);
   });
 });
